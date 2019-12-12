30a2390d1db8715367c2cfde7e6859d2f0c9d40e
Hannah Stover <hrosestover@gmail.com>
2019-12-12 12:49:30 -0800

    Add files via upload

---
+++ public/precache-manifest.bfa86a83e5094f103a92120e2bfc4812.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adaeac1ef63d918c09b985285416e5ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60e55d5eff63edab68ab",
+    "url": "/static/css/2.c59c4e42.chunk.css"
+  },
+  {
+    "revision": "611eec8ab112834147fc",
+    "url": "/static/css/main.506eba59.chunk.css"
+  },
+  {
+    "revision": "60e55d5eff63edab68ab",
+    "url": "/static/js/2.b35d1c27.chunk.js"
+  },
+  {
+    "revision": "611eec8ab112834147fc",
+    "url": "/static/js/main.688a3e7e.chunk.js"
+  },
+  {
+    "revision": "8870a603714713bb2741",
+    "url": "/static/js/runtime-main.17759ccd.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
